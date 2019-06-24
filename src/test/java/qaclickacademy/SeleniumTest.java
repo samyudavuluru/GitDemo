05ee843b6da74a7a172ee4b672b1bd0ac6a20818
samyu <samyu812@gmail.com>
2019-06-24 11:49:17 -0400

    first commit

---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,21 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void BrowserAutomation()
+	{
+	System.out.println("BrowserAutomation");
+	}
+    @Test
+    public void elemntsUI()
+    {
+    	System.out.println("elementsUI");
+	}
+    
+    
+   
+    
+}
